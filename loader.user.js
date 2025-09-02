cf8db5c4bfc87a0496fdad99e12e72a7c6e6c143
krystianasaaa <krycha022005@gmail.com>
2025-09-02 16:08:46 +0200

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -9,6 +9,7 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @run-at       document-start
+// @icon        https://raw.githubusercontent.com/krystianasaaa/margonem-addons/b939ec05fdd03f6f973cef7a931659c224596bde/ikonka.png
 // @updateURL    https://krystianasaaa.github.io/margonem-addons/loader.user.js
 // @downloadURL  https://krystianasaaa.github.io/margonem-addons/loader.user.js
 // ==/UserScript==
